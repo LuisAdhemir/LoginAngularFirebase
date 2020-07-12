189bc3908abaa106280edab1c4aaee89c7fc9f58
Luis Añazgo <luis.anazgo@taxtech.pe>
2020-07-12 09:01:48 -0500

    Firebase Config Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCfmEdwEybQqsfdarW_kj6jtVn61adh3Gc",
+    authDomain: "login-firebase-angular.firebaseapp.com",
+    databaseURL: "https://login-firebase-angular.firebaseio.com",
+    projectId: "login-firebase-angular",
+    storageBucket: "login-firebase-angular.appspot.com",
+    messagingSenderId: "408304197801",
+    appId: "1:408304197801:web:7c8e1970f4a1eb2cff89b8"
+  }
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,24 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCfmEdwEybQqsfdarW_kj6jtVn61adh3Gc",
+    authDomain: "login-firebase-angular.firebaseapp.com",
+    databaseURL: "https://login-firebase-angular.firebaseio.com",
+    projectId: "login-firebase-angular",
+    storageBucket: "login-firebase-angular.appspot.com",
+    messagingSenderId: "408304197801",
+    appId: "1:408304197801:web:7c8e1970f4a1eb2cff89b8"
+  }
+  // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
+
+
 };
+
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
